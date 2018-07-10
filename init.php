367dc0a41a40c6bef71380312366c2c78bf2a3b2
Karel Zavadil <mojemejly@centrum.cz>
2018-07-10 13:20:46 +0000

    multi

--- init.php
+++ init.php
@@ -2,11 +2,13 @@
 	require_once __DIR__ . '/../zEngine/src/zengine.php';	
 	$z = new zEngine(__DIR__ . '/app/');	
 	$z->enableModule('mysql');
-	
+
+	$db = $z->mysql->connection;
+
 	$sql = file_get_contents(__DIR__ . '/../zEngine/sql/zEngine.sql');
-	zSqlQuery::executeSQL($z->mysql->connection, $sql);
+	$db->multi_query($sql);
 	
 	$sql = file_get_contents(__DIR__ . '/sql/sample.sql');
-	zSqlQuery::executeSQL($z->mysql->connection, $sql);
+	$db->multi_query($sql);
 
 	echo "Database initialized.";
