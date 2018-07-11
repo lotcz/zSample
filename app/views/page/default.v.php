b34d0b8084f0d60866f4c667549dc65f58a5363b
Karel Zavadil <mojemejly@centrum.cz>
2018-07-11 21:59:28 +0000

    cs

--- app/views/page/default.v.php
+++ app/views/page/default.v.php
@@ -1,7 +1,7 @@
 <h1><?=$this->t('Hello') ?></h1>
 
-<h2>This is <strong>zEngine</strong> sample app.</h2>
+<h2><?=$this->t('This is <strong>zEngine</strong> sample app.') ?></h2>
 
 <p>
-	This app is basic test and showcase of <strong>zEngine</strong> features.
+	<?=$this->t('This app is basic test and showcase of <strong>zEngine</strong> features.') ?>
 </p>
