6c548849b5ef6589128afdd120889e923185b61b
Karel Zavadil <mojemejly@centrum.cz>
2018-08-08 17:14:18 +0000

    added more sudos

--- init.php
+++ init.php
@@ -3,15 +3,18 @@
 	if ($argc < 2) {
 		die("Please provide mySQL server root user name and password as parameters. Example: init.php root rootpass");
 	}
-	
+
 	$root_user = $argv[1];
-	$root_password = $argv[2];
-	
+	$root_password = '';
+	if (isset($argv[2])) {
+		$root_password = $argv[2];
+	}
+
 	$dbconfig = include __DIR__ . '/app/config/mysql.php';
 
 	$hostname = $dbconfig['db_host'];
 	$dbname = $dbconfig['db_name'];
-	
+
 	$command = "mysql --default-character-set=utf8 -h $hostname -D $dbname --user=$root_user --password='$root_password' < ";
 
 	$sql_path = __DIR__ . '/../zEngine/sql/zEngine.sql';
@@ -21,8 +24,8 @@
 	$sql_path = __DIR__ . '/sql/sample.sql';
 	$output = shell_exec($command . $sql_path);
 	echo $output;
-	
-	require_once __DIR__ . '/../zEngine/src/zengine.php';	
-	$z = new zEngine(__DIR__ . '/app/');	
+
+	require_once __DIR__ . '/../zEngine/src/zengine.php';
+	$z = new zEngine(__DIR__ . '/app/');
 	$z->enableModule('mysql');
 	echo "Database initialized.";
