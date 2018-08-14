15ee8f8a842eb77892d7a688186dbbeab882ac11
Karel Zavadil <mojemejly@centrum.cz>
2018-08-14 13:11:41 +0000

    composer + phpunit + selenium

--- tests/process/visitorLoginTest.php
+++ tests/process/visitorLoginTest.php
@@ -2,21 +2,25 @@
 
 class VisitorLoginTest extends PHPUnit_Extensions_Selenium2TestCase {
 
-    public function setUp() {
-        $this->setHost('localhost');
-        $this->setPort(4444);
-        $this->setBrowserUrl($_SERVER['ZSAMPLE_TEST_URL']);
-        $this->setBrowser('firefox');
-    }
+  protected $captureScreenshotOnFailure = TRUE;
+  protected $screenshotPath = '/var/www/screenshots';
+  protected $screenshotUrl = 'http://localhost/screenshots';
+  
+  public function setUp() {
+    $this->setHost('localhost');
+    $this->setPort(4444);
+    $this->setBrowserUrl($_SERVER['ZSAMPLE_TEST_URL']);
+    $this->setBrowser('firefox');
+  }
 
-    public function tearDown() {
-        $this->stop();
-    }
+  public function tearDown() {
+    $this->stop();
+  }
 
-    public function testFrontPage() {
-      $this->url('/');
-      $content = $this->byClass('main-title')->text();
-      $this->assertEquals('Hello', $content);
+  public function testFrontPage() {
+    $this->url('/');
+    $content = $this->byClass('main-title')->text();
+    $this->assertEquals('Hello', $content);
   }
 
 }
