517db4d5f8c412d5e992f4b6d5e3d336dd564acf
Karel Zavadil <mojemejly@centrum.cz>
2018-08-16 16:10:52 +0000

    no selenium server

--- app/views/page/default.v.php
+++ app/views/page/default.v.php
@@ -3,3 +3,13 @@
 <p>
 	<?=$this->t('This app is basic test and showcase of <strong>zEngine</strong> features.') ?>
 </p>
+
+<?php
+
+$unique_string = str_replace(".", "", strtoupper(uniqid(uniqid(uniqid(), true), true))) ;
+echo $unique_string . '<br/>';
+$unique_string = str_replace(".", "", strtoupper(uniqid(uniqid(uniqid(), true), true))) ;
+echo $unique_string . '<br/>';
+$unique_string = str_replace(".", "", strtoupper(uniqid(uniqid(uniqid(), true), true))) ;
+echo $unique_string . '<br/>';
+?>
--- tests/process/visitorLoginTest.php
+++ tests/process/visitorLoginTest.php
@@ -5,10 +5,10 @@ class VisitorLoginTest extends PHPUnit_Extensions_Selenium2TestCase {
   protected $captureScreenshotOnFailure = TRUE;
   protected $screenshotPath = '/var/www/screenshots';
   protected $screenshotUrl = 'http://localhost/screenshots';
-  
+
   public function setUp() {
-    $this->setHost('localhost');
-    $this->setPort(4444);
+    //$this->setHost('localhost');
+    //$this->setPort(4444);
     $this->setBrowserUrl($_SERVER['ZSAMPLE_TEST_URL']);
     $this->setBrowser('firefox');
   }
